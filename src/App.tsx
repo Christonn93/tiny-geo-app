91f8c300ae1655595d06d42d5eb5c357de3b71f3
Christopher <46559129+Christonn93@users.noreply.github.com>
2025-06-05 20:41:47 +0200

    removing npm and using bun

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import GeoLocationCard from "./components/GeoLocationCard";
 import { PrivacyNotice } from "./components/PrivacyNotice";
-import { AppWrapper } from "./components/AppWrapper";
 import { Button } from "./components/Button";
+import { AppContainer } from "./components/AppContainer";
 
 function App() {
   const [showLocation, setShowLocation] = useState(false);
 
   return (
-    <AppWrapper>
+    <AppContainer>
       {!showLocation && <Button setShowLocation={() => setShowLocation(true)} />}
       <div className="w-full flex justify-center">{showLocation && <GeoLocationCard />}</div>
       <div className="align-self-bottom">
         <PrivacyNotice />
       </div>
-    </AppWrapper>
+    </AppContainer>
   );
 }
 
